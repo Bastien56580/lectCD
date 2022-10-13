1c3319b5b0bd03e7f41c827ebcd7759187ee7491
Bastien <bastien.lesueur56@gmail.com>
2022-10-13 22:05:55 +0200

    ihm

---
+++ src/ihm/FrameLectCD.java
@@ -0,0 +1,22 @@
+package ihm;
+
+import javax.swing.JFrame;
+
+public class interfaceCD extends JFrame {
+    public interfaceCD(){
+        super("interface CD");
+    }
+    public static void main (String[] args){
+    MyWindow myWindow = new MyWindow();
+
+
+
+
+
+
+
+    }
+
+
+
+}
