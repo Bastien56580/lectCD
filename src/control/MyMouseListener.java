46b9aec03759207ff10521f947b278cce1f98d22
Bastien <bastien.lesueur56@gmail.com>
2022-10-13 22:51:18 +0200

    remise à zero du control

--- src/control/MyMouseListener.java
+++ src/control/MyMouseListener.java
@@ -1,22 +1,13 @@
 package control;
 
-import datas.Duree;
 import ihm.FrameLectCD;
 
+import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.File;
 
 public class MyMouseListener implements ActionListener {
-
-    private final FrameLectCD frameLectCD;
-
-    private final Duree duree;
-
-
-    public MyMouseListener(FrameLectCD frameLectCD, Duree duree) {
-        this.frameLectCD = frameLectCD;
-        this.duree = duree;
-    }
 
     @Override
     public void actionPerformed(ActionEvent e) {
@@ -24,5 +15,3 @@ public class MyMouseListener implements ActionListener {
     }
 }
 
-
-}
